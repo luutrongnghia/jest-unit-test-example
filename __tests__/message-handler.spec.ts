3adf8378bfd153e0acd21f1a621e0240903a9e6f
luutrongnghia <luutrongnghia38@gmail.com>
2023-08-14 15:56:50 +0700

    test

--- __tests__/message-handler.spec.ts
+++ __tests__/message-handler.spec.ts
@@ -23,29 +23,29 @@ describe("Message Handler", () => {
     jest.clearAllMocks();
   });
 
-  it("should be send Hello, World!", async () => {
-    message.content = "!hello";
-    await messageHandler(message);
-    expect(message.channel.send).toHaveBeenCalledWith("Hello, World!");
-    expect(message.channel.send).not.toHaveBeenCalledWith("Help Command");
-  });
+  // it("should be send Hello, World!", async () => {
+  //   message.content = "!hello";
+  //   await messageHandler(message);
+  //   expect(message.channel.send).toHaveBeenCalledWith("Hello, World!");
+  //   expect(message.channel.send).not.toHaveBeenCalledWith("Help Command");
+  // });
 
-  it("should be send Help Command", async () => {
-    message.content = "!help";
-    await messageHandler(message);
-    expect(message.channel.send).toHaveBeenCalledTimes(1);
-    expect(message.channel.send).toHaveBeenCalledWith("Help Command");
-  });
+  // it("should be send Help Command", async () => {
+  //   message.content = "!help";
+  //   await messageHandler(message);
+  //   expect(message.channel.send).toHaveBeenCalledTimes(1);
+  //   expect(message.channel.send).toHaveBeenCalledWith("Help Command");
+  // });
 
-  it("should be throw an error when a bot sends a message", async () => {
-    message.author.bot = true;
-    try {
-      await messageHandler(message);
-    } catch (err) {
-      expect(err).toBeDefined();
-      expect(message.channel.send).not.toHaveBeenCalled();
-    }
-  });
+  // it("should be throw an error when a bot sends a message", async () => {
+  //   message.author.bot = true;
+  //   try {
+  //     await messageHandler(message);
+  //   } catch (err) {
+  //     expect(err).toBeDefined();
+  //     expect(message.channel.send).not.toHaveBeenCalled();
+  //   }
+  // });
   
   // it("should be not call any channel.send()", async () => {
   //   message.author.bot = false;
